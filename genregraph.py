2b0e806f6c42b454e4c903c57b6ab52b28d79733
rhaskia <rhaskiagamedev@gmail.com>
2024-12-12 11:04:42 +1300

    working graph-tool graphs

--- genregraph.py
+++ genregraph.py
@@ -1,11 +1,21 @@
 import matplotlib.pyplot as plt
 import eyed3
 import os
-import graph_tool.all as gt
+import string
 
-directory = "E:/music"
+directory = "/mnt/e/Music/"
 files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 data = {}
+
+def clean(str1):
+    no_whitespace = ''.join(str1.split()).lower()
+
+    no_punc = ''.join(char for char in no_whitespace if char not in string.punctuation)
+
+    if no_punc.endswith("music"):
+        return no_punc[:-5]
+    else: 
+        return no_punc
 
 # minimum string alphabetically
 def min(str1, str2):
@@ -20,7 +30,7 @@ def max(str1, str2):
 
 
 for file in files: 
-    audiofile = eyed3.load("E:/music/" + file)
+    audiofile = eyed3.load("/mnt/e/Music/" + file)
 
     if not audiofile: 
         continue
@@ -31,6 +41,8 @@ for file in files:
         genres = genre.name.split("\0")
         for genre1 in genres: 
             for genre2 in genres:
+                genre1 = clean(genre1)
+                genre2 = clean(genre2)
                 firstgenre = min(genre1, genre2)
                 secondgenre = max(genre1, genre2)
                 key = (firstgenre, secondgenre)
@@ -38,36 +50,8 @@ for file in files:
 
 print(data)
 
-data = [(k[0], k[1], v) for k, v in data.items() if v > 10 and k[0] != k[1]]
+data = [(k[0], k[1], v) for k, v in data.items() if k[0] != k[1]]
 
+with open('weights.txt', 'w') as file:
+    file.writelines(['' + line[0] + ',' + line[1] + ',' + str(line[2]) + '\n' for line in data])
 
-def create_graph_tool_graph(node_weights):
-    g = gt.Graph(directed=False)  # Create an undirected graph
-
-    # Create a property map for edge weights
-    eweight = g.new_edge_property("float")
-
-    # Add vertices to the graph
-    vertices = set()
-    for source, target, weight in node_weights:
-        vertices.add(source)
-        vertices.add(target)
-    vertex_map = {v: g.add_vertex() for v in vertices}
-
-    # Add edges with weights
-    for source, target, weight in node_weights:
-        source_vertex = vertex_map[source]
-        target_vertex = vertex_map[target]
-        e = g.add_edge(source_vertex, target_vertex)
-        eweight[e] = weight
-
-    return g, eweight
-
-g, eweight = create_graph_tool_graph(data)
-
-# Visualize the graph (basic example)
-pos = gt.sfdp_layout(g) 
-gt.graph_draw(g, pos=pos, edge_pen_width=eweight) 
-
-# You can now use graph-tool's features to analyze and visualize the graph
-# (e.g., community detection, shortest paths, centrality measures)
---
+++ grapher.py
@@ -0,0 +1,52 @@
+import csv
+import os
+from graph_tool.all import *
+
+g = Graph(directed=False)  # Create an undirected graph
+v_name = g.new_vertex_property("string")
+vertices = {}
+
+def get_vert(g, d, vert): 
+    if vert not in d:
+        d[vert] = g.add_vertex()
+        print(vert, d[vert])
+    return d[vert]
+
+with open("weights.txt", 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # Skip the header row if present
+
+    # Create edge properties
+    e_weight = g.new_edge_property("float") 
+
+    for row in reader:
+        source, target, weight = row
+        if int(weight) < 3: continue
+
+        v1 = get_vert(g, vertices, source)
+        v_name[v1] = source
+        v2 = get_vert(g, vertices, target)
+        v_name[v2] = target
+
+        g.add_edge(v1, v2)
+        e_weight[g.edge(v1, v2)] = 1.0 / float(weight)
+
+state = minimize_blockmodel_dl(g)
+vb, eb = betweenness(g)
+
+# Define a function to display vertex name on hover
+def on_hover_vertex(v_index):
+    if v_index is not None:
+        v_name = vertex_names[v_index]
+        v_inspector.set_text(v_name)
+    else:
+        v_inspector.set_text("")
+
+pos = sfdp_layout(g, eweight=e_weight, C=0.3,r=2,p=2)
+
+state.draw(output="graph.pdf", pos=pos, vertex_text=g.vertex_index)
+
+# Visualize the graph (basic example)
+# graph_draw(g, vertex_text=g.vertex_index,
+#            vertex_fill_color=prop_to_size(vb, 0, 1, power=.1), vertex_size=prop_to_size(vb, 3, 12, power=.2), vorder=vb, output="graph-sbm.pdf")
+
