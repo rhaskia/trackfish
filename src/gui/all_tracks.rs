9e665dfa34e344f7b6b5c1d8eb6add39b7556a12
rhaskia <rhaskiagamedev@gmail.com>
2025-04-23 20:31:08 +1200

    working virtualization for all tracks view

--- src/gui/all_tracks.rs
+++ src/gui/all_tracks.rs
@@ -1,6 +1,8 @@
 use super::{View, VIEW};
 use crate::app::utils::strip_unnessecary;
 use dioxus::prelude::*;
+use dioxus::document::eval;
+use log::info;
 
 fn display_time(total: u64) -> String {
     let seconds = total % 60;
@@ -25,6 +27,64 @@ pub fn AllTracks() -> Element {
             .sum::<f64>() as u64
     });
 
+    let mut window_size = use_signal(|| 0);
+    const ROW_HEIGHT: usize = 62;
+    const BUFFER_ROWS: usize = 5;
+
+    let mut start_index = use_signal(|| 0);
+    let rows_in_view = use_memo(move || window_size() / ROW_HEIGHT + BUFFER_ROWS);
+    let end_index = use_memo(move || (start_index() + rows_in_view()).min(tracks.read().len()));
+    let mut scroll = use_signal(|| 0);
+
+    use_effect(move || {
+        let mut js = eval(
+            r#"
+            document.addEventListener('mousemove', function(event) {
+                let container = document.getElementById('alltrackslist');
+                dioxus.send(container.offsetHeight);
+            });
+            addEventListener('resize', function(event) {
+                let container = document.getElementById('alltrackslist');
+                dioxus.send((container.offsetHeight);
+            });
+        "#,
+        );
+
+        spawn(async move {
+            loop {
+                let height = js.recv::<usize>().await;
+                if let Ok(height) = height {
+                    window_size.set(height);
+                    info!("{height}");
+                }
+            }
+        });
+    });
+
+    use_effect(move || {
+        let mut js = eval(
+            r#"
+            let container = document.getElementById('alltrackslist');
+            container.addEventListener('scroll', function(event) {
+                dioxus.send(container.scrollTop);
+            });
+        "#,
+        );
+
+        spawn(async move {
+            loop {
+                let scroll_top = js.recv::<usize>().await;
+                if let Ok(scroll_top) = scroll_top {
+                    let new_index = (scroll_top as f32 / ROW_HEIGHT as f32).floor() as usize;
+                    if new_index != start_index() {
+                        start_index.set(new_index);
+                    }
+                info!("hi");
+                }
+            }
+        });
+    });
+
     rsx! {
         div {
             class: "alltracksview",
@@ -37,11 +97,18 @@ pub fn AllTracks() -> Element {
                 "{CONTROLLER.read().all_tracks.len()} songs / "
                 "{display_time(total_time())} total duration"
             }
-            div { class: "tracklist",
-                for i in 0..CONTROLLER.read().all_tracks.len().min(100) {
+            div { class: "tracklist", id: "alltrackslist",
+                position: "relative",
+
+                div { 
+                    min_height: "{(tracks.read().len()) * ROW_HEIGHT}px",
+                }
+
+                for i in start_index()..end_index() {
                     div {
                         class: "trackitem",
                         id: "alltracks-trackitem-{i}",
+                        style: "top: {i * ROW_HEIGHT}px; position: absolute;",
                         onclick: move |_| {
                             CONTROLLER.write().add_all_queue(i);
                             VIEW.write().current = View::Song;
@@ -60,6 +127,7 @@ pub fn AllTracks() -> Element {
                         }
                     }
                 }
+
             }
             if is_searching() {
                 TracksSearch { tracks, is_searching, id_prefix: "alltracks" }
